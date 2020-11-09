d5e72bcb5ae3dafc592498ac858e222c89e72200
n73274246 <nisaisik@std.iyte.edu.tr>
2020-11-09 09:40:45 +0000

    new

--- lab4/example4.py
+++ lab4/example4.py
@@ -1,5 +1,6 @@
 a = int(input("Write a number "))
 b = int(input("Write another one "))
-for i in b:
-  result = a
-print(result)
+power = 1
+for i in range(1,b+1):
+  power = power * a
+print(power)
