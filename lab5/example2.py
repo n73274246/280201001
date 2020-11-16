478ad1d5c71b48138589199766761200bc7f1891
n73274246 <nisaisik@std.iyte.edu.tr>
2020-11-16 09:15:07 +0000

    done

--- lab5/example2.py
+++ lab5/example2.py
@@ -1,9 +1,8 @@
-number_ofinteger = input("How many numbers are there? ")
+number_ofinteger = int(input("How many numbers are there? "))
 count = 0
-for i in number_ofinteger:
+for i in range(number_ofinteger):
   n = int(input("Enter a number "))
   if n %2 == 0:
     count = count + 1
-  number_ofinteger = int(number_ofinteger)
   percent = count/number_ofinteger * 100
 print(percent)
