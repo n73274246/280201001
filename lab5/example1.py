c057d23ee33ec9f2edef1f64ba861766e169e250
n73274246 <nisaisik@std.iyte.edu.tr>
2020-11-16 08:58:35 +0000

    negative

--- lab5/example1.py
+++ lab5/example1.py
@@ -1,4 +1,9 @@
 number = int(input("Write a number"))
-for i in range(11):
-  solution = number*i
-  print(solution)
+if number >0:
+  for i in range(11):
+    solution = number*i
+    print(solution)
+else:
+  print("Please enter a valid number")
+  
+  
